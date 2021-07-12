f3a0ed42a9228643cd9ef8dd42b8079022ea70ff
brito101 <rodrigobrito101@hotmail.com>
2021-07-11 22:11:12 -0300

    Refactoring numbers-animation

--- js/modules/numbers-animation.js
+++ js/modules/numbers-animation.js
@@ -1,31 +1,45 @@
-export default function numbersAnimation() {
-  function initAnimation() {
-    const numbers = document.querySelectorAll("[data-number]");
-    numbers.forEach((number) => {
-      const total = +number.innerText;
-      const increment = Math.floor(total / 100);
-      let start = 0;
-      const counter = setInterval(() => {
-        start += increment;
-        number.innerText = start;
-        if (start > total) {
-          number.innerText = total;
-          clearInterval(counter);
-        }
-      }, 25);
-    });
+export default class NumbersAnimation {
+  constructor(numbers, observerTarget, observerClass) {
+    this.numbers = document.querySelectorAll(numbers)
+    this.observerTarget = document.querySelector(observerTarget)
+    this.observerClass = observerClass
+    this.handleMutation = this.handleMutation.bind(this)
   }
 
-  let observer;
+  static incrementNumbers(number) {
+    const total = +number.innerText
+    const increment = Math.floor(total / 100)
+    let start = 0
+    const counter = setInterval(() => {
+      start += increment
+      number.innerText = start
+      if (start > total) {
+        number.innerText = total
+        clearInterval(counter)
+      }
+    }, 25)
+  }
 
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("active")) {
-      observer.disconnect();
-      initAnimation();
+  initAnimation() {
+    this.numbers.forEach((number) => this.constructor.incrementNumbers(number))
+  }
+
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect()
+      this.initAnimation()
     }
   }
 
-  observer = new MutationObserver(handleMutation);
-  const observerTarget = document.querySelector(".numbers");
-  observer.observe(observerTarget, { attributes: true });
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation)
+    this.observer.observe(this.observerTarget, { attributes: true })
+  }
+
+  init() {
+    if (this.numbers.length && this.observerTarget) {
+      this.addMutationObserver()
+    }
+    return this
+  }
 }
--- js/modules/numbers-fetch.js
+++ js/modules/numbers-fetch.js
@@ -1,28 +1,31 @@
-import numbersAnimation from "./numbers-animation.js";
+import NumbersAnimation from "./numbers-animation.js"
 
 export default function numbersFetch() {
   function createNumberDiv(number) {
-    const div = document.createElement("div");
-    div.classList.add("numbers-skill");
-    div.innerHTML = `<h3>${number.name}</h3><span data-number>${number.total}</span>`;
-    return div;
+    const div = document.createElement("div")
+    div.classList.add("numbers-skill")
+    div.innerHTML = `<h3>${number.name}</h3><span data-number>${number.total}</span>`
+    return div
   }
 
   async function apiFetch(url) {
     try {
-      const apiResponse = await fetch(url);
-      const apiJSON = await apiResponse.json();
-      const numbersGrid = document.querySelector(".numbers-grid");
+      const apiResponse = await fetch(url)
+      const apiJSON = await apiResponse.json()
+      const numbersGrid = document.querySelector(".numbers-grid")
 
       apiJSON.forEach((item) => {
-        const divNumber = createNumberDiv(item);
-        numbersGrid.appendChild(divNumber);
-      });
-      numbersAnimation();
+        const divNumber = createNumberDiv(item)
+        numbersGrid.appendChild(divNumber)
+      })
+
+      const animaNumbers = new NumbersAnimation("[data-number]", ".numbers", "active");
+      animaNumbers.init();
+
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
-  apiFetch("./numbers-api.json");
+  apiFetch("./numbers-api.json")
 }
