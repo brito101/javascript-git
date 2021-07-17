9ee96f822fb6971ba6a0525ea58c3386c36c0a5c
brito101 <rodrigobrito101@hotmail.com>
2021-07-16 23:27:48 -0300

    Refatoring Numbers Fetch

--- js/modules/numbers-fetch.js
+++ js/modules/numbers-fetch.js
@@ -1,6 +1,6 @@
 import NumbersAnimation from "./numbers-animation.js"
 
-export default function numbersFetch() {
+export default function numbersFetch(url, target) {
   function createNumberDiv(number) {
     const div = document.createElement("div")
     div.classList.add("numbers-skill")
@@ -8,24 +8,32 @@ export default function numbersFetch() {
     return div
   }
 
-  async function apiFetch(url) {
+  const numbersGrid = document.querySelector(target)
+
+  function insertData(item) {
+    const divNumber = createNumberDiv(item)
+    numbersGrid.appendChild(divNumber)
+  }
+
+  function numbersAnimation() {
+    const animaNumbers = new NumbersAnimation(
+      "[data-number]",
+      ".numbers",
+      "active"
+    )
+    animaNumbers.init()
+  }
+
+  async function apiFetch() {
     try {
       const apiResponse = await fetch(url)
       const apiJSON = await apiResponse.json()
-      const numbersGrid = document.querySelector(".numbers-grid")
-
-      apiJSON.forEach((item) => {
-        const divNumber = createNumberDiv(item)
-        numbersGrid.appendChild(divNumber)
-      })
-
-      const animaNumbers = new NumbersAnimation("[data-number]", ".numbers", "active");
-      animaNumbers.init();
-
+      apiJSON.forEach((item) => insertData(item))
+      numbersAnimation()
     } catch (error) {
       console.log(error)
     }
   }
 
-  apiFetch("./numbers-api.json")
+  return apiFetch()
 }
--- js/script.js
+++ js/script.js
@@ -1,41 +1,42 @@
-import SmoothScroll from "./modules/smooth-scroll.js";
-import Accordion from "./modules/accordion.js";
-import TabNav from "./modules/tab-nav.js";
-import Modal from "./modules/modal.js";
-import Tooltip from "./modules/tooltip.js";
+import SmoothScroll from "./modules/smooth-scroll.js"
+import Accordion from "./modules/accordion.js"
+import TabNav from "./modules/tab-nav.js"
+import Modal from "./modules/modal.js"
+import Tooltip from "./modules/tooltip.js"
 
-import animationScroll from "./modules/animation-scroll.js";
-import dropdownMenu from "./modules/dropdown-menu.js";
-import menuMobile from "./modules/menu-mobile.js";
-import openingHours from "./modules/opening-hours.js";
-import numbersFetch from "./modules/numbers-fetch.js";
-import bitcoinFetch from "./modules/bitcoin-fetch.js";
+import animationScroll from "./modules/animation-scroll.js"
+import dropdownMenu from "./modules/dropdown-menu.js"
+import menuMobile from "./modules/menu-mobile.js"
+import openingHours from "./modules/opening-hours.js"
+import numbersFetch from "./modules/numbers-fetch.js"
+import bitcoinFetch from "./modules/bitcoin-fetch.js"
 
-const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"]');
-smoothScroll.init();
+const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"]')
+smoothScroll.init()
 
-const accordion = new Accordion('[data-anime="accordion"] dt');
-accordion.init();
+const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init()
 
 const tabNav = new TabNav(
   '[data-tab="menu"] li',
   '[data-tab="content"] section'
-);
-tabNav.init();
+)
+tabNav.init()
 
 const modal = new Modal(
   '[data-modal="open"]',
   '[data-modal="close"]',
   '[data-modal="container"]'
-);
-modal.init();
+)
+modal.init()
 
-const tooltip = new Tooltip("[data-tooltip]");
-tooltip.init();
+const tooltip = new Tooltip("[data-tooltip]")
+tooltip.init()
 
-animationScroll();
-dropdownMenu();
-menuMobile();
-openingHours();
-numbersFetch();
-bitcoinFetch();
+numbersFetch("../../numbers-api.json", ".numbers-grid")
+
+animationScroll()
+dropdownMenu()
+menuMobile()
+openingHours()
+bitcoinFetch()
